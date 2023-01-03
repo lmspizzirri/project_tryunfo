bd20caf1504312874632bf4822c3c39e0b46695b
lmspizzirri <lm.spizzirri@gmail.com>
2023-01-03 13:27:09 -0500

    Req 3

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+      let superTrunfo;
+    if (cardTrunfo) {
+      superTrunfo = <p data-testid="trunfo-card">Super Trunfo</p>;
+    }
+    return (
+      <div>
+        <p data-testid="name-card">${cardName}</p>
+        <img src={cardImage} alt={cardName} data-testid="image-card" />
+        <p data-testid="description-card">{cardDescription}</p>
+        <p data-testid="attr1-card">{cardAttr1}</p>
+        <p data-testid="attr2-card">{cardAttr2}</p>
+        <p data-testid="attr3-card">{cardAttr3}</p>
+        <p data-testid="rare-card">{cardRare}</p>
+        {superTrunfo};
+      </div>
+    )
+  }
+}
